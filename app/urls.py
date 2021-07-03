c0b3b5a8d2fde6459dbb626a5e5594f84e8e9337
Nicolás Vidal <nic.vidalm@duocuc.cl>
2021-07-03 03:38:09 -0400

    Se agrega carpeta static con sus respectivas subcarpetas

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, contacto, productos
 
 urlpatterns = [
     path('', home, name="home"),
+    path('contacto/', contacto, name="contacto"),
+    path('productos/', productos, name="productos"),
 ]
--- app/views.py
+++ app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(render, 'app/home.html')
+    return render(request, 'app/home.html')
+
+def contacto(request):
+    return render(request, 'app/contacto.html')
+
+def productos(request):
+    return render(request, 'app/productos.html')
