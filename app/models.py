3ab8c0a5f7f41aa7a1673e95a3579fe7f33de2a0
Nicolás Vidal <nic.vidalm@duocuc.cl>
2021-07-03 05:12:37 -0400

    Se agregan los modelos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    tipo = models.ForeignKey(Tipo, on_delete=models.PROTECT)
+    fecha_fabricacion = models.DateField()
+
+    def __str__(self):
+        return self.nombre
