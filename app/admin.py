f32821772ec2a0de22b79086892b08e00ccb8181
Nicolás Vidal <nic.vidalm@duocuc.cl>
2021-07-03 16:41:01 -0400

    Se personaliza el sitio administrativo de la pasteleria

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Tipo, Producto
 
 # Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "precio", "tipo", "fecha_fabricacion"]
+
 admin.site.register(Tipo)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
