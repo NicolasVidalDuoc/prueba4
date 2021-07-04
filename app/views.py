fcd6276576abfd756fa8f9b24ccbf2ddc836512f
Nicolás Vidal <nic.vidalm@duocuc.cl>
2021-07-04 13:56:53 -0400

    Se agrega el modificar y eliminar productos

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
 from .forms import ContactoForm, ProductoForm
 
@@ -39,3 +39,19 @@ def listar_productos(request):
     productos = Producto.objects.all()
     data = {'productos': productos}
     return render(request, 'app/producto/listar.html', data)
+
+def modificar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    data = {'form': ProductoForm(instance=producto)}
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to="listar_productos")
+        data["form"] = formulario
+    return render(request, 'app/producto/modificar.html', data)
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    return redirect(to="listar_productos")
