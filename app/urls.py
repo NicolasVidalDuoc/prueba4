152accc30ccaedf9ba975f870e71e271eddd3076
Nicolás Vidal <nic.vidalm@duocuc.cl>
2021-07-04 00:08:31 -0400

    Se agrega el listar y agregar un producto de la pasteleria

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, contacto, productos
+from .views import home, contacto, productos, agregar_producto, listar_productos
 
 urlpatterns = [
     path('', home, name="home"),
     path('contacto/', contacto, name="contacto"),
     path('productos/', productos, name="productos"),
+    path('agregar-producto/', agregar_producto, name="agregar_producto"),
+    path('listar-productos/', listar_productos, name="listar_productos"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Producto
-from .forms import ContactoForm
+from .forms import ContactoForm, ProductoForm
 
 # Create your views here.
 def home(request):
@@ -22,3 +22,20 @@ def contacto(request):
 
 def productos(request):
     return render(request, 'app/productos.html')
+
+def agregar_producto(request):
+    data = {'form': ProductoForm()}
+
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "guardado correctamente"
+        else:
+            data["form"] = formulario
+    return render(request, 'app/producto/agregar.html', data)
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    data = {'productos': productos}
+    return render(request, 'app/producto/listar.html', data)
